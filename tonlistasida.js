02eb0bc08a2712e071b864a947740a1a6a8703a2
einargisla6 <einarorvar@gmail.com>
2025-09-24 10:04:02 +0000

    24. september

--- tonlistasida.js
+++ tonlistasida.js
@@ -1,7 +1,21 @@
 function getData(){
-  axios.get("https://www.theaudiodb.com/api/v1/json")
-      .then(response => {
-        console.log(response.data);
+  var texti = document.getElementById("leit").value
+
+  axios.get("https://www.theaudiodb.com/api/v1/json" + texti)
+      .then(function (response) {
+        var artist = response.data;
+        var app = document.getElementById("app")
+        for (var i = 0; i < artist.length; i++) {
+            var div = document.createElement("div")
+            var h1 = document.createElement("h1")
+            var img = document.createElement("img")
+            var h2 = document.createElement("h2")
+            var p = document.createElement("p")
+            var ahtml = document.createElement("a")
+            var p2 = document.createElement("p")
+            var p1 = document.createElement("p")
+            var p3 = document.createElement("p")
+        }
       })
       .catch(error => {
         console.error("Error fetching:", error);
