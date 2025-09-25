9f23ce20a76c4439cdf8ae911ea7aac63d885a25
einargisla6 <einarorvar@gmail.com>
2025-09-25 11:07:52 +0000

    25. september

--- tonlistasida.js
+++ tonlistasida.js
@@ -1,23 +1,30 @@
 function getData(){
   var texti = document.getElementById("leit").value
 
-  axios.get("https://www.theaudiodb.com/api/v1/json" + texti)
+  axios.get("https://www.theaudiodb.com/api/v1/json/123/search.php?s=" +texti)
       .then(function (response) {
-        var artist = response.data;
+        var artists = response.data.artists;
         var app = document.getElementById("app")
-        for (var i = 0; i < artist.length; i++) {
+        for (var i = 0; i < artists.length; i++) {
             var div = document.createElement("div")
             var h1 = document.createElement("h1")
             var img = document.createElement("img")
             var h2 = document.createElement("h2")
             var p = document.createElement("p")
-            var ahtml = document.createElement("a")
             var p2 = document.createElement("p")
             var p1 = document.createElement("p")
             var p3 = document.createElement("p")
+            if (parseInt(artists[i].intMembers) > 1) {
+              h1.innerHTML = artists[i].strArtist + " - " + "Band";
+              img.src = artists[i].strArtistCutout
+              div.append(h1)
+              div.append(img)
+              app.append(div)
+            }
         }
       })
       .catch(error => {
         console.error("Error fetching:", error);
       });
 }
+//strArtist, strStyle, strGenre, strBiography, strArtistCutout
